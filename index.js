4922373bd832c15ebb0f5557fff59a4a46f7462e
oguzcan <oguzakkuss@gmail.com>
2023-02-18 21:34:52 +0300

    Port change

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({hi: 'there' });
+    res.send({hi: 'THERE' });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
 app.listen(PORT);
