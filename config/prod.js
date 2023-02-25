2f74f5ea1f3228957895d013b738e9e0ca13d59e
oguzcan <oguzakkuss@gmail.com>
2023-02-25 23:13:14 +0300

    prod fix

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,3 @@
-// keys.js - figure out what set credentials to return
 const { SecretClient } = require("@azure/keyvault-secrets");
 const { DefaultAzureCredential } = require("@azure/identity");
 
@@ -7,13 +6,16 @@ const keyVaultName = "oguzakkus-keys";
 const url = "https://" + keyVaultName + ".vault.azure.net";
 const client = new SecretClient(url, credential);
 
+async function getSecrets() {
+    return {
+        googleClientID: await client.getSecret('GOOGLE-CLIENT-ID'),
+        googleClientSecret: await client.getSecret('GOOGLE-CLIENT-SECRET'),
+        mongoURI: await client.getSecret('MONGO-URI'),
+        cookieKey: await client.getSecret('COOKIE-KEY'),
+        certKeyName: await client.getSecret('CERTIFICATE-KEY'),
+        certName: await client.getSecret('CERTIFICATE'),
+        PORT: '443'
+    };
+}
 
-module.exports = {
-    googleClientID: await client.getSecret('GOOGLE-CLIENT-ID'),
-    googleClientSecret: await client.getSecret('GOOGLE-CLIENT-SECRET'),
-    mongoURI: await client.getSecret('MONGO-URI'),
-    cookieKey: await client.getSecret('COOKIE-KEY'),
-    certKeyName: await client.getSecret('CERTIFICATE-KEY'),
-    certName: await client.getSecret('CERTIFICATE'),
-    PORT: '443'
-};
+module.exports = getSecrets;
