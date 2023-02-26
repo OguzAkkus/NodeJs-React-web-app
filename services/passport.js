11e2460de02ed5dd57492e5fa9257216e7cf68f3
oguzcan <oguzakkuss@gmail.com>
2023-02-26 19:23:10 +0300

    Async

--- services/passport.js
+++ services/passport.js
@@ -5,28 +5,25 @@ const User = mongoose.model('users');
 const { getSecrets } = require('../config/keys');
 
 
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-
-passport.deserializeUser((id, done) => {
-  User.findById(id).then(user => {
-    done(null, user);
-  });
-});
-
-
-
 (async () => {
   const secrets = await getSecrets();
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => {
+      done(null, user);
+    });
+  });
+
   passport.use(
     new GoogleStrategy(
       {
         
         clientID: secrets.googleClientID,
         clientSecret: secrets.googleClientSecret,
-        callbackURL: '/auth/google/callback',
-        proxy: true
+        callbackURL: '/auth/google/callback'
       },
       (accessToken, refreshToken, profile, done) => {
         User.findOne({ googleId: profile.id }).then((existingUser) => {
