e5ca875611d6b67df9525f8517421ea981de3bf6
oguzcan <oguzakkuss@gmail.com>
2023-03-09 15:40:36 +0300

    Survey Forms Validate

--- client/src/utils/validateEmails.js
+++ client/src/utils/validateEmails.js
@@ -1,4 +1,4 @@
-const re = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/
+const re = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
 
 export default (emails) => {
   const invalidEmails = emails
